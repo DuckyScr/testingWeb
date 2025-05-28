ce482b3e1ad2becfe651aebbfaadb7109ceadd7f
Adam Ragac <drumer.ragi420@gmail.com>
2025-05-28 13:01:15 +0200

    fix

--- src/app/dashboard/import/page.tsx
+++ src/app/dashboard/import/page.tsx
@@ -5,7 +5,7 @@ export default function DevPage() {
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">In Development</h1>
       <p className="text-gray-500 dark:text-gray-400 mb-4">
-        Import is in Sevelopment.
+        Import is in Development.
       </p>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-export default function Home() {
+'use client';
 
- //redirect to /login if not logged in
-  if (!localStorage.getItem('token')) {
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
     window.location.href = '/login';
-  }
+  }, []);
 
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">In Development</h1>
       <p className="text-gray-500 dark:text-gray-400 mb-4">
-        Import is in Sevelopment.
+        Import is in Development.
       </p>
     </div>
   );
